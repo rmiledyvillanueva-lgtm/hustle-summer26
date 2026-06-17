13696081d55d0480b5655ee9ce90babdef040a7b
Rosa Miledy Villanueva Villa <miledyvillanueva@Rosas-MacBook-Pro.local>
2026-06-17 14:43:02 -0700

    Add Python variables lab

---
+++ profile.py
@@ -0,0 +1,14 @@
+Full_name = "Rosa Miledy Villanueva Villa"
+nickname = "Mili"
+age = 18
+used_python_before = False
+hobbies = ["hanging out with friends", "reading", "hicking"]
+favorites = {
+    "color": "Blue",
+    "food": "Quesadillas",
+    "fruit": "Mango"
+}
+print( Full_name, age, hobbies, favorites )
+languages_spoken = {"Spanish", "English"}
+skills = ("teamwork", "communication")
+print( languages_spoken, skills, used_python_before, nickname)
---
+++ variables.py
@@ -0,0 +1,4 @@
+First_name = "miledy"
+last_name = "villa"
+age = 19
+print (First_name, last_name, age)
